66f7d3b7a80ad59af0d6c71f30a03a7765c52975
dotan <dotan53@gmail.com>
2025-02-05 01:17:58 -0600

    Add files via upload

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -80,8 +80,8 @@ SELECT
     employees.first_name AS "First Name",
     departments.dept_name AS "Department Name"
 FROM employees
-JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_name IN ('Sales', 'Development');
 
 --List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
@@ -92,3 +92,6 @@ SELECT
 FROM employees
 GROUP BY employees.last_name
 ORDER BY "Frequency" DESC;
+
+
+
